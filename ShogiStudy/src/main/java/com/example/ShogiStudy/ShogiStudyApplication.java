2d620c785f34f921041d69d768e91ea43fd29f1f
wieourew <kodai0715.94@gmail.com>
2023-05-26 14:05:14 +0900

    Initial commit

---
+++ ShogiStudy/src/main/java/com/example/ShogiStudy/ShogiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShogiStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShogiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShogiStudyApplication.class, args);
+	}
+
+}
---
+++ ShogiStudy/src/test/java/com/example/ShogiStudy/ShogiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShogiStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShogiStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
